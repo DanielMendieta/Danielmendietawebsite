d3970ca66bf11d68995e8ae25e08ecae858f3af8
DanielMendieta <110202705+DanielMendieta@users.noreply.github.com>
2022-12-29 21:29:32 -0300

    Update views.py

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Proyect
-from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -9,3 +11,15 @@ def home(request):
 def proyectos(request):
     projects=Proyect.objects.all()
     return render (request, 'proyectos.html',{'projects':projects} )    
+
+def formulario(request):
+    if request.method=='POST':
+        subject = request.POST["asunto"]
+        message = request.POST["message"] + " " + request.POST["email"]
+        email_from= settings.EMAIL_HOST_USER
+        recipient_list = ["mendietadaniel1994@gmail.com"]
+        send_mail (subject, message, email_from, recipient_list)
+        messages.success(request,'Tu correo ha sido enviado gracias❤')
+        return render (request, 'formulario.html')
+
+    return render(request, 'formulario.html')    
